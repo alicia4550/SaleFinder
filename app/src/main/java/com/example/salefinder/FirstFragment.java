4e0d666154eb1285f7b02238486b2a51b1dc921f
Alicia Tran <aliciatran4550@gmail.com>
2023-10-04 18:35:47 -0400

    feat: create opening screen to add items to search

--- app/src/main/java/com/example/salefinder/FirstFragment.java
+++ app/src/main/java/com/example/salefinder/FirstFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.salefinder.databinding.FragmentFirstBinding;
+import com.example.salefinder.ui.adapter.AddedItemsAdapter;
+import com.example.salefinder.ui.model.AddedItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private AddedItemsAdapter addedItemsAdapter;
+    private List<AddedItem> addedItems;
 
     @Override
     public View onCreateView(
@@ -29,13 +38,27 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        RecyclerView recyclerAddedItems = (RecyclerView) getView().findViewById(R.id.recycler_added_items);
+        addedItems = new ArrayList<>();
+        addedItemsAdapter = new AddedItemsAdapter(addedItems);
+        // Attach the adapter to the recyclerview to populate items
+        recyclerAddedItems.setAdapter(addedItemsAdapter);
+        // Set layout manager to position the items
+        recyclerAddedItems.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        binding.addItemBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                EditText editText_item = (EditText) getView().findViewById(R.id.edit_text_item);
+                String itemText = editText_item.getText().toString();
+                System.out.println(itemText);
+                editText_item.setText("");
+
+                addedItems.add(0, new AddedItem(itemText));
+                addedItemsAdapter.notifyItemInserted(0);
             }
         });
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/salefinder/ui/model/AddedItem.java
@@ -0,0 +1,15 @@
+package com.example.salefinder.ui.model;
+
+import java.util.ArrayList;
+
+public class AddedItem {
+    private String name;
+
+    public AddedItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
